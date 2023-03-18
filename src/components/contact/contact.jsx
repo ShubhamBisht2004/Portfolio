83817df5c959eceb2e85afedc15506f2e449cc59
Shubham Bisht <SHIV.BISHT20@GMAIL.COM>
2023-03-18 21:31:36 +0530

    first commit

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,19 @@
+// import { sendForm } from 'emailjs-com';
+
+// const form = document.getElementById("contact-form");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     const serviceId = "service_4dfak5x";
+//     const templateId = "template_vy05oni";
+//     const userId = "SddJZOnegLhdzj-pX";
+
+//     sendForm(serviceId, templateId, form.current, userId)
+//         .then(function(response) {
+//             console.log("SUCCESS!", response.status, response.text);
+//             form.reset();
+//         }, function(error) {
+//             console.log("FAILED...", error);
+//         });
+// });
